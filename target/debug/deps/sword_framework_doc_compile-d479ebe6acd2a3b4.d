c831d11110e0c3ffe7a098a0bfdfdb2efd1ef19f
seho <1018715564@qq.com>
2022-09-15 22:15:04 +0800

    🎉 init: rust project init

---
+++ target/debug/deps/sword_framework_doc_compile-d479ebe6acd2a3b4.d
@@ -0,0 +1,5 @@
+/Users/seho/Desktop/openSource/sword-framework-doc-compile/target/debug/deps/sword_framework_doc_compile-d479ebe6acd2a3b4.rmeta: src/main.rs
+
+/Users/seho/Desktop/openSource/sword-framework-doc-compile/target/debug/deps/sword_framework_doc_compile-d479ebe6acd2a3b4.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/sword_framework_doc_compile-e67c6d167ebeb8a4.d
@@ -0,0 +1,5 @@
+/Users/seho/Desktop/openSource/sword-framework-doc-compile/target/debug/deps/sword_framework_doc_compile-e67c6d167ebeb8a4.rmeta: src/main.rs
+
+/Users/seho/Desktop/openSource/sword-framework-doc-compile/target/debug/deps/sword_framework_doc_compile-e67c6d167ebeb8a4.d: src/main.rs
+
+src/main.rs:
