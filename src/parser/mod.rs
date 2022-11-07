1988cec14ea2f1e6335948a436b0243f6ac7609a
seho <seho.zaq@gmail.com>
2022-11-07 22:17:54 +0800

    🎈 perf: change the incoming parameter to file

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,13 +1,11 @@
-use std::fs; 
 use pest::{iterators::Pair, Parser};
 use pest_derive::Parser;
 #[derive(Parser)]
 #[grammar = "parser/proto.pest"] // relative to src
 pub struct TypeScriptParser;
 
-pub fn parse(path: &str) {
-    let file = fs::read_to_string(path).unwrap();
-    let pairs = TypeScriptParser::parse(Rule::, &file).unwrap_or_else(|e| panic!("{}", e));
+pub fn parse(file: &str) {
+    let pairs = TypeScriptParser::parse(Rule::proto, &file).unwrap_or_else(|e| panic!("{}", e));
     for pair in pairs {
         println!("{:?}", pair);
     }
---
+++ tests/src/proto.test.ts
@@ -0,0 +1,5 @@
+export interface ReqParams { }
+export interface ReqQuery { }
+export interface Res {
+    message: string;
+}
